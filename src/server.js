05f0c6a93b59948b11b3859795d4a7b5af64a253
MichelePesanteDev <michele.pesante.dev@gmail.com>
2025-09-07 00:18:19 +0200

    Updated server logic

--- src/server.js
+++ src/server.js
@@ -6,14 +6,17 @@ wss.on('connection', (ws) => {
     console.log('New client connected');
 
     ws.on('message', (message) => {
-        console.log(`Received: ${message}`);
-        // Echo the message back to the client
-        ws.send(`Server received: ${message}`);
+        console.log('Received:', message);
+        if (message.key === 'topic') {
+            wss.clients.forEach((client) => {
+                if (client.readyState === client.OPEN) {
+                    client.send('Il topic scelto è: ' + message.body);
+                }
+            });
+        }
     });
 
     ws.on('close', () => {
         console.log('Client disconnected');
     });
 });
-
-console.log('WebSocket server is running on ws://localhost:8080');
