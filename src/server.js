bf0d1f8faa224504c2ab84819f5d531738356b5d
MichelePesanteDev <michele.pesante.dev@gmail.com>
2025-09-07 21:51:36 +0200

    Updated server logic

--- src/server.js
+++ src/server.js
@@ -13,10 +13,12 @@ wss.on('connection', (ws) => {
             const data = JSON.parse(text);
             console.log('Parsed object:', data);
 
-            if (data.key === 'topic') {
+            if (data.key === 'startMatch') {
                 wss.clients.forEach((client) => {
                     if (client.readyState === client.OPEN) {
-                        client.send('Il topic scelto è: ' + data.body);
+                        client.send('Il topic scelto è: ' + data.topic);
+                        client.send('La scelta 1 è: ' + data.firstChoice);
+                        client.send('La scelta 2 è: ' + data.secondChoice);
                     }
                 });
             }
