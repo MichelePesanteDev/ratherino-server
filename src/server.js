4e74672e7d5f53c60a71da326ebfeb8406ad59c5
MichelePesanteDev <michele.pesante.dev@gmail.com>
2025-09-13 02:08:43 +0200

    Updated server logic

--- src/server.js
+++ src/server.js
@@ -1,33 +1,32 @@
-const WebSocket = require('ws');
+const WebSocket = require("ws");
 
 const wss = new WebSocket.Server({ port: 8080 });
 
-wss.on('connection', (ws) => {
-    console.log('New client connected');
+wss.on("connection", (ws) => {
+  console.log("New client connected");
 
-    ws.on('message', (message) => {
-        const text = message.toString();
-        console.log('Raw message:', text);
+  ws.on("message", (message) => {
+    const text = message.toString();
+    console.log("Raw message:", text);
 
-        try {
-            const data = JSON.parse(text);
-            console.log('Parsed object:', data);
+    try {
+      const data = JSON.parse(text);
+      console.log("Parsed object:", data);
 
-            if (data.key === 'startMatch') {
-                wss.clients.forEach((client) => {
-                    if (client.readyState === client.OPEN) {
-                        client.send('Il topic scelto è: ' + data.topic);
-                        client.send('La scelta 1 è: ' + data.firstChoice);
-                        client.send('La scelta 2 è: ' + data.secondChoice);
-                    }
-                });
-            }
-        } catch (err) {
-            console.error('Messaggio non in formato JSON:', err);
-        }
-    });
+      if (data.key === "updateInfo") {
+        wss.clients.forEach((client) => {
+          if (client.readyState === client.OPEN) {
+            console.log("Loggando per un client:", data);
+            client.send(text);
+          }
+        });
+      }
+    } catch (err) {
+      console.error("Messaggio non in formato JSON:", err);
+    }
+  });
 
-    ws.on('close', () => {
-        console.log('Client disconnected');
-    });
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
 });
